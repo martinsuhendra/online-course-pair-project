1dffda146d41954ca19023e4a3460d0fdb42d757
martinsuhendra <suhendra.martin90@gmail.com>
2019-03-29 05:34:25 +0700

    deploying

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const home = require('./routes')
 const loginRoutes = require('./routes/login')
 const registerRoutes = require('./routes/register')
